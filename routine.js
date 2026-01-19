e6c0821a3eb3b99cd66394848da0d7bb80d7a5b0
D-Koganov <47898672+D-Koganov@users.noreply.github.com>
2026-01-19 09:40:05 +0300

    Update routine.js

--- routine.js
+++ routine.js
@@ -1,6 +1,6 @@
 var dnow = new Date();
 dnow.setHours(8); //setting hours, 'cause we want only days
-var devent = new Date(2025, 11, 30, 8); //months start from 0
+var devent = new Date(2025, 04, 15, 8); //months start from 0
 
 var DEBUG = false;
 
